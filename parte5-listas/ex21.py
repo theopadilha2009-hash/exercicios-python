303d45cd41a39e9d083bccb98633cc48a0cdda08
Theo Lorentz Padilha <theopadilha2009@gmail.com>
2026-09-22 15:31:00 -0300

    Implementa exercicio 21: criacao e exibicao de lista com 5 elementos

---
+++ parte5-listas/ex21.py
@@ -0,0 +1,7 @@
+# Exercício 21 - Listas
+# Enunciado: Crie uma lista com cinco números à sua escolha e exiba cada um deles.
+
+lista_numeros = [14, 28, 35, 42, 56]
+
+for numero in lista_numeros:
+    print(numero)
