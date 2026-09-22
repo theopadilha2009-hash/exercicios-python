5ef1dfbab963c03464efef16623bbde4b031a8f8
Theo Lorentz Padilha <theopadilha2009@gmail.com>
2026-09-22 11:51:00 -0300

    Implementa exercicio 01: declaracao de variaveis com nome e idade

---
+++ parte1-variaveis/ex01.py
@@ -0,0 +1,8 @@
+# Exercício 01 - Variáveis, entrada e saída
+# Enunciado: Crie duas variáveis com seu nome e sua idade. Exiba cada uma em uma linha.
+
+nome_completo = "Theo Lorentz Padilha"
+idade_anos = 16
+
+print(nome_completo)
+print(idade_anos)
