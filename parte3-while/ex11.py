5d4d59e3c73dc413f33eee1ec55554e15f34e877
Theo Lorentz Padilha <theopadilha2009@gmail.com>
2026-09-22 13:41:00 -0300

    Implementa exercicio 11: contagem progressiva de 1 a 10 com while

---
+++ parte3-while/ex11.py
@@ -0,0 +1,8 @@
+# Exercício 11 - Repetição com while
+# Enunciado: Exiba os números de 1 a 10, um por linha.
+
+numero_atual = 1
+
+while numero_atual <= 10:
+    print(numero_atual)
+    numero_atual += 1
