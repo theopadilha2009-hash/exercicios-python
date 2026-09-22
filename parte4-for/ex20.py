0619c060435eda22b33ea0c56a87e512d2fab5c3
Theo Lorentz Padilha <theopadilha2009@gmail.com>
2026-09-22 15:20:00 -0300

    Implementa exercicio 20: contagem regressiva de 10 a 1 com for

---
+++ parte4-for/ex20.py
@@ -0,0 +1,7 @@
+# Exercício 20 - Repetição com for
+# Enunciado: Faça uma contagem regressiva de 10 até 1 e, ao final, exiba "Fim".
+
+for segundo_restante in range(10, 0, -1):
+    print(segundo_restante)
+
+print("Fim")
