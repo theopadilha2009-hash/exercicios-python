a45c86d8cdc9e36e013979a94a31deeb7c3d30c6
Theo Lorentz Padilha <theopadilha2009@gmail.com>
2026-09-22 16:04:00 -0300

    Implementa exercicio 24: contagem de itens maiores que 10 em lista

---
+++ parte5-listas/ex24.py
@@ -0,0 +1,12 @@
+# Exercício 24 - Listas
+# Enunciado: Dada a lista [5, 12, 8, 20, 3, 15], informe quantos itens são maiores que 10.
+
+lista_dados = [5, 12, 8, 20, 3, 15]
+
+contador_maiores_que_dez = 0
+
+for numero in lista_dados:
+    if numero > 10:
+        contador_maiores_que_dez += 1
+
+print(f"Quantidade de itens maiores que 10: {contador_maiores_que_dez}")
