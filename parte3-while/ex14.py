0447133de1437d1d76e485dddf47418db0babfb0
Theo Lorentz Padilha <theopadilha2009@gmail.com>
2026-09-22 14:14:00 -0300

    Implementa exercicio 14: tabuada de multiplicacao de 1 a 10 com while

---
+++ parte3-while/ex14.py
@@ -0,0 +1,10 @@
+# Exercício 14 - Repetição com while
+# Enunciado: Peça um número e exiba sua tabuada de 1 a 10.
+
+numero_base = int(input("Digite um número para ver sua tabuada: "))
+multiplicador = 1
+
+while multiplicador <= 10:
+    resultado_multiplicacao = numero_base * multiplicador
+    print(f"{numero_base} x {multiplicador} = {resultado_multiplicacao}")
+    multiplicador += 1
