685f06110bb5a7369d61c7219b918b4f602d3d91
Theo Lorentz Padilha <theopadilha2009@gmail.com>
2026-09-22 12:57:00 -0300

    Implementa exercicio 07: comparacao de maioridade entre dois numeros

---
+++ parte2-condicionais/ex07.py
@@ -0,0 +1,12 @@
+# Exercício 07 - Condicionais
+# Enunciado: Peça dois números e exiba qual é o maior. Se forem iguais, informe isso.
+
+primeiro_numero = float(input("Digite o primeiro número: "))
+segundo_numero = float(input("Digite o segundo número: "))
+
+if primeiro_numero > segundo_numero:
+    print(f"O maior número é: {primeiro_numero}")
+elif segundo_numero > primeiro_numero:
+    print(f"O maior número é: {segundo_numero}")
+else:
+    print("Os dois números informados são iguais.")
