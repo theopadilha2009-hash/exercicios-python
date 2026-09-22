4e9f806151b180e7d2eac72e7bdbbc319ab9caf8
Theo Lorentz Padilha <theopadilha2009@gmail.com>
2026-09-22 12:46:00 -0300

    Implementa exercicio 06: verificacao de paridade de um numero

---
+++ parte2-condicionais/ex06.py
@@ -0,0 +1,9 @@
+# Exercício 06 - Condicionais
+# Enunciado: Peça um número e informe se ele é par ou ímpar.
+
+numero_informado = int(input("Digite um número inteiro: "))
+
+if numero_informado % 2 == 0:
+    print(f"O número {numero_informado} é par.")
+else:
+    print(f"O número {numero_informado} é ímpar.")
